ec530537af0b38d8630e427f8d48abf079df4d38
Donovan Said <donovansaid@DonMacBook.local>
2020-03-14 19:01:48 +0000

    Started working on the cli interface.

--- src/cli_interface.py
+++ src/cli_interface.py
@@ -3,3 +3,35 @@
 
 """TBC
 """
+
+
+import logging
+import argparse
+
+
+HTTP = "http://"
+""" HTTP
+"""
+
+HTTPS = "https://"
+""" HTTPS
+"""
+
+LOGGER = logging.getLogger()
+""" LOGGER
+"""
+
+LOGGER.setLevel(logging.DEBUG)
+
+def user_interface():
+    """TBC
+    """
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument('--url', action='store', type=str, required=True,
+                           help="The URL that you want to test.")
+
+    args = my_parser.parse_args()
+
+    return args
+
+print(user_interface())
