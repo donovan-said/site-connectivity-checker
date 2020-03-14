f06abbc1134f0206e69fe23171bdbe04311b656e
Donovan Said <donovansaid@DonMacBook.local>
2020-03-14 19:32:38 +0000

    Added .pylintrc file. Started working main.py.

--- src/cli_interface.py
+++ src/cli_interface.py
@@ -21,17 +21,27 @@ LOGGER = logging.getLogger()
 """ LOGGER
 """
 
+# Setting log level.
 LOGGER.setLevel(logging.DEBUG)
 
-def user_interface():
-    """TBC
+def arg_parser():
+    """A function to manage cli arguments.
+
+    Arguments:
+        None.
+
+    Returns:
+        User input.
     """
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument('--url', action='store', type=str, required=True,
                            help="The URL that you want to test.")
+    my_parser.add_argument('--port', action='store', type=str, required=False,
+                           help="The port associated to the endpoint.")
 
     args = my_parser.parse_args()
 
     return args
 
-print(user_interface())
+if __name__ == "__main__":
+    pass
--- src/main.py
+++ src/main.py
@@ -3,3 +3,14 @@
 
 """TBC
 """
+
+from cli_interface import arg_parser
+
+
+def wrapper():
+    """TBC
+    """
+    print(arg_parser())
+
+if __name__ == "__main__":
+    wrapper()
